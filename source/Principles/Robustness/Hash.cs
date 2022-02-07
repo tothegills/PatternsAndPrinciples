d616265361be687c9b36ecd38a49873919e37119
Shane Gill <tothegills@gmail.com>
2022-02-07 11:39:47 +1000

    Robustness principle

---
+++ source/Principles/Robustness/Hash.cs
@@ -0,0 +1,54 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Principles.Robustness;
+
+public class Hash
+{
+    readonly SHA256 sha = SHA256.Create();
+
+    public IEnumerable<byte> GetHash(string source)
+    {
+        return sha.ComputeHash(Encoding.UTF8.GetBytes(source)).Select(b => b);
+    }
+
+    // liberal input
+    public IEnumerable<byte> GetHash(byte[] bytes)
+    {
+        return sha.ComputeHash(bytes).Select(b => b);
+    }
+
+    // liberal input +
+    public IEnumerable<byte> GetHash(IEnumerable<byte> bytes)
+    {
+        return sha.ComputeHash(bytes.ToArray()).Select(b => b);
+    }
+
+    // conservative output
+    public byte[] GetHash_ConservativeOutput(IEnumerable<byte> bytes)
+    {
+        return sha.ComputeHash(bytes.ToArray());
+    }
+
+    // conservative output +
+    public string GetHash_ConservativeOutput_Plus(IEnumerable<byte> bytes)
+    {
+        return BitConverter.ToString(sha.ComputeHash(bytes.ToArray()));
+    }
+
+    // conservative output ++
+    public HashValue GetHash_ConservativeOutput_PlusPlus(IEnumerable<byte> bytes)
+    {
+        return new HashValue(sha.ComputeHash(bytes.ToArray()));
+    }
+
+    public class HashValue
+    {
+        public string Value { get; }
+
+        public HashValue(byte[] value)
+        {
+            Value = BitConverter.ToString(value);
+        }
+    }
+}
---
+++ source/Principles/Robustness/README.md
@@ -0,0 +1,7 @@
+# Robustness Principle (Postel's law)
+
+> Be conservative in what you do, be liberal in what you accept from others
+
+This can also be expressed as:
+
+> Be contravariant in the input type and covariant in the output type
